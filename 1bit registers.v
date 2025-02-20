18ebf4589fa5593b011de338a9ef30d852dd0a87
cagedmotion <cagedmotion@gmail.com>
2025-02-19 20:11:47 -0500

    update

--- 1bit registers.v
+++ 1bit registers.v
@@ -12,11 +12,8 @@ module register_1bit (
             dout = din;
         end
     end
-<<<<<<< HEAD
-
 
 endmodule
-
 
 module tb_1reg();
 
@@ -35,8 +32,5 @@ module tb_1reg();
         reset = 1'b0; din = 1'b1; en = 1'b1; #PERIOD; #PERIOD;
     end
 endmodule
-=======
-endmodule
 
-// Test
->>>>>>> 26a55ec5431e542d639c19c79fd50554e3f9ef76
+
