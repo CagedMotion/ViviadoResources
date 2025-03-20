0133a08657289fd712fe24f8686cfa799472fd3c
Drew Thompson <138719198+CagedMotion@users.noreply.github.com>
2025-03-19 21:26:44 -0400

    updated

--- Execute-Memory_writeback register.v
+++ Execute-Memory_writeback register.v
@@ -1,9 +1,9 @@
 module Exe_Mem_WB_reg(
     input wire clk, reset,
     input wire gp_reg_wb_in,
-    input wire [9:0] ram_rdata_in,
+    input wire [9:0] ram_rdata_in, alu_result_in,
     input wire [2:0] gp_rdata2_address_in,
-    output wire [9:0] ram_rdata_out, 
+    output wire [9:0] ram_rdata_out, alu_result_out,
     output wire [2:0] gp_rdata2_address_out,
     output wire gp_reg_wb_out
     );
@@ -11,7 +11,8 @@ module Exe_Mem_WB_reg(
     wire en;
     assign en = 1'b1;
     
-    register_10bit REG1(.clk(clk), .reset(reset), .en(en), .dout(ram_rdata_out), .din(ram_rdata_in));
+    register_10bit REG1(.clk(clk), .reset(reset), .en(en), .dout(ram_rdata_out), .din(ram_rdata_in)),
+                   REG2(.clk(clk), .reset(reset), .en(en), .dout(alu_result_out), .din(alu_result_in));
 
     register_1bit REG3(.clk(clk), .reset(reset), .en(en), .dout(gp_reg_wb_out), .din(gp_reg_wb_in)),
                   REG4(.clk(clk), .reset(reset), .en(en), .dout(gp_rdata2_address_out[2]), .din(gp_rdata2_address_in[2])),
