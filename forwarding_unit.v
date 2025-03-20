c11f457a6a3d13cab80352ccc01a64603c8b2d92
SpiteSlays <grahamedidin@icloud.com>
2025-03-19 20:00:31 -0400

    Forwarding update

--- forwarding_unit.v
+++ forwarding_unit.v
@@ -4,6 +4,7 @@ module forwarding_unit (
     input  wire [9:0]  ex_dest_reg_value, // Value of the Destination register after the execute/memory stage'
     input  wire [2:0]  id_dest_reg,   // First source register from the Decode stage.
     input  wire [2:0]  id_src_reg,   // Second source register from the Decode stage.
+    input  wire        wb,          // Write Back enable bit
     output reg         forwardA,          // Selector bit for Forward A Mux
     output reg         forwardB          // Selector bit for Forward B Mux
 
@@ -12,14 +13,20 @@ module forwarding_unit (
     always @(*) begin
         // If a load instruction in the execute stage writes to a register that is
         // needed by the instruction in the decode stage, assert stall.
-        if (((exmem_wb_wr == 1'b1) & (ex_dest_reg_value == 10'b0)) & (ex_dest_reg == id_dest_reg)) begin
-            forwardA = 1'b1;
-        end else begin
-            forwardA = 1'b0;
+        if (wb == 1'b1) begin
+            if (((exmem_wb_wr == 1'b1) & (ex_dest_reg_value == 10'b0)) & (ex_dest_reg == id_dest_reg)) begin
+                forwardA = 1'b1;
+            end else begin
+                forwardA = 1'b0;
+            end
+            if (((exmem_wb_wr == 1'b1) & (ex_dest_reg_value == 10'b0)) & (ex_dest_reg == id_src_reg)) begin
+                forwardB = 1'b1;
+            end else begin
+                forwardB = 1'b0;
+            end
         end
-        if (((exmem_wb_wr == 1'b1) & (ex_dest_reg_value == 10'b0)) & (ex_dest_reg == id_src_reg)) begin
-            forwardB = 1'b1;
-        end else begin
+        else begin
+            forwardA = 1'b0;
             forwardB = 1'b0;
         end
     end
